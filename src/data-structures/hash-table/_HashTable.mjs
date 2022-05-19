1ef1414e1322915fb04b22285da5045b315e3499
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2022-05-19 21:34:27 +1000

    Add hash_table get & set methods

--- src/data-structures/hash-table/_HashTable.mjs
+++ src/data-structures/hash-table/_HashTable.mjs
@@ -1,4 +1,5 @@
 import LinkedList from '../linked-list/LinkedList';
+import LinkedListNode from '../linked-list/LinkedListNode';
 
 // Hash table size directly affects on the number of collisions.
 // The bigger the hash table size the less collisions you'll get.
@@ -22,5 +23,49 @@ export default class HashTable {
       return prev + current.charCodeAt();
     }, 0);
     return hashValue % this.buckets.length;
+  }
+
+  set(key, value) {
+    const node = new LinkedListNode({ key, value });
+    const hashKey = this.hash(key);
+    this.keys[key] = hashKey;
+    const bucket = this.buckets[hashKey];
+    if (!bucket.head) {
+      bucket.head = node;
+      bucket.tail = bucket.head;
+      return node;
+    }
+
+    let currentNode = bucket.head;
+    if (currentNode.value.key === key) {
+      currentNode.value.value = value;
+      return node;
+    }
+    while (currentNode.next && currentNode.next.value.value < value) {
+      if (currentNode.next.value.key === key) {
+        currentNode.next.value.value = value;
+      }
+      currentNode = currentNode.next;
+    }
+    node.next = currentNode.next;
+    currentNode.next = node;
+    return node;
+  }
+
+  get(key) {
+    const hashKey = this.hash(key);
+    const bucket = this.buckets[hashKey];
+    if (!bucket.head) {
+      return undefined;
+    }
+    let currentNode = bucket.head;
+    while (currentNode && currentNode.value.key !== key) {
+      currentNode = currentNode.next;
+    }
+    return currentNode ? currentNode.value.value : undefined;
+  }
+
+  has(key) {
+    return Object.hasOwnProperty.call(this.keys, key);
   }
 }
