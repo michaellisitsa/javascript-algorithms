2adbf8aedaa26562d88b1af9cbe4873c2fd48ef4
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2022-05-21 22:45:30 +1000

    Use comparator in Insertion Sort

--- src/algorithms/sorting/insertion-sort/_InsertionSort.mjs
+++ src/algorithms/sorting/insertion-sort/_InsertionSort.mjs
@@ -5,7 +5,7 @@ export default class InsertionSort extends Sort {
     const array = [...originalArray];
     for (let i = 0; i < array.length; i += 1) {
       let j = i;
-      while (j > 0 && array[j] < array[j - 1]) {
+      while (j > 0 && this.comparator.lessThan(array[j], array[j - 1])) {
         [array[j], array[j - 1]] = [array[j - 1], array[j]];
         j -= 1;
       }
