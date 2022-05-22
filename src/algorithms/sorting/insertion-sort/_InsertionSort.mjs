fccdb076cca78aebd6a1cfe20830fe67cdce8755
Michael Lisitsa <michael.lisitsa@gmail.com>
2022-05-22 14:22:20 +1000

    Insertion sort test now tracks how many times function is called

--- src/algorithms/sorting/insertion-sort/_InsertionSort.mjs
+++ src/algorithms/sorting/insertion-sort/_InsertionSort.mjs
@@ -3,8 +3,10 @@ import Sort from '../_Sort.mjs';
 export default class InsertionSort extends Sort {
   sort(originalArray) {
     const array = [...originalArray];
-    for (let i = 0; i < array.length; i += 1) {
+    for (let i = 1; i < array.length; i += 1) {
       let j = i;
+      // Call visiting callback
+      this.callbacks.visitingCallback();
       while (j > 0 && this.comparator.lessThan(array[j], array[j - 1])) {
         [array[j], array[j - 1]] = [array[j - 1], array[j]];
         j -= 1;
